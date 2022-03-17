60b3379f5345dcf8e91915bc296f70b56dffec68
JustWork Robot <robot@just-work.org>
2022-03-18 02:52:39 +0300

    CR-fix-2

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -17,9 +17,7 @@ def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
-      'group': group,
-      'posts': posts,
+        'group': group,
+        'posts': posts,
     }
     return render(request, 'posts/group_list.html', context)
-
-# Create your views here.
