f2573257b93431e50efebb9cbdb4972b498a509d
Сергеев Виталий <serg3evvitaly@yandex.ru>
2022-03-21 21:23:01 +0300

    3 отправка на ревью

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,9 +4,10 @@ from django.shortcuts import get_object_or_404
 from .models import Post
 from .models import Group
 
+date = 10
 
 def index(request):
-    posts = Post.objects.order_by('-pub_date')[:10]
+    posts = Post.objects.order_by('-pub_date')[:date]
     context = {
         'posts': posts,
     }
@@ -15,7 +16,7 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:date]
     context = {
         'group': group,
         'posts': posts,
