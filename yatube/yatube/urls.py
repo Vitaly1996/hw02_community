535e65961499712d432dbce33936cb0455eea82d
Сергеев Виталий <serg3evvitaly@yandex.ru>
2022-03-24 00:37:05 +0300

    Актуальная версия

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
 
 urlpatterns = [
     path('', include('posts.urls', namespace='posts')),
-    path('admin/', admin.site.urls)
+    path('auth/', include('users.urls'), namespace='users'),
+    path('auth/', include('django.contrib.auth.urls')),
+    path('admin/', admin.site.urls),
 ]
